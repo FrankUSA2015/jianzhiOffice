65cfd2442fddf14f82444d1ab5850cd3f7639d17
FrankUSA2015 <2019682602@qq.com>
2020-04-24 23:13:49 +0800

    2020-4-24

---
+++ 27.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def mirrorTree(self, root: TreeNode) -> TreeNode:
